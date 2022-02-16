1828f715c4b0d255f4286e8ec407928415887b1b
TestAuthor <exaple@gmail.com>
2022-02-16 11:48:03 +0300

    dddd

--- Sources/MyViewPackge/MyViewPackge.swift
+++ Sources/MyViewPackge/MyViewPackge.swift
@@ -1,6 +1,30 @@
+import UIKit
 public struct MyViewPackge {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+}
+
+
+public class MyView {
+    
+    var view = UIView()
+    let sizeScreen = UIScreen.main.bounds
+    
+    init(){}
+    
+    public func position(x:CGFloat,y:CGFloat){
+        view = UIView(frame: CGRect(x: x, y: y, width: sizeScreen.midX / 2, height: sizeScreen.midX / 2))
+        view.layer.cornerRadius = 10
+    }
+    
+    public func backgroundColor(color:UIColor){
+        view.backgroundColor = color
+        
+    }
+    
+    public func addView()->UIView{
+        return view
     }
 }
